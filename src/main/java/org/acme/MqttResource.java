aa9de36761be5a66eb5890a9036c7103b0ee71f5
mpcbass <mpcbass@gmail.com>
2021-01-11 13:00:43 +0100

    in/out examples

--- src/main/java/org/acme/MqttResource.java
+++ src/main/java/org/acme/MqttResource.java
@@ -1,20 +1,32 @@
 package org.acme;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.subscription.Cancellable;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.time.Duration;
 import java.util.Random;
+import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class MqttResource {
 
     private Random random = new Random();
 
-    @Outgoing("prices")
+
+    @Outgoing("out-prices")
     public Multi<Double> generate() {
         return Multi.createFrom().ticks().every(Duration.ofSeconds(1))
                 .map(x -> random.nextDouble()).onFailure().retry().indefinitely();
     }
+/*
+    @Incoming("in-prices")
+    public CompletionStage<Void> receive(Message<String> message) {
+        return message.ack();
+    }
+     */
 }
